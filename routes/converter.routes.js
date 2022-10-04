6b02b2a8e52e4372041595a23615825e3bc0ae42
hugotata78 <hugotatarinoff@gmail.com>
2022-10-04 17:40:10 -0300

    corrección parametros ruta conversor de monedas

--- routes/converter.routes.js
+++ routes/converter.routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express')
 const { currencyConverter, getSymbols } = require('../controllers/converter.controllers')
 const router = Router()
 
-router.get('/:from/:to/:amount', currencyConverter)
+router.get('/:to/:from/:amount', currencyConverter)
 router.get('/',getSymbols)
 
 module.exports = router
